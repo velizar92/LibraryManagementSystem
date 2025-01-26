022221d6fb0ca185892c100943445a15db61875a
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2025-01-26 20:22:29 +0200

    Remove two methods from BorrowedBookRepository

--- LibraryManagementSystem.DataAccessLibrary/Repositories/IBookRepository.cs
+++ LibraryManagementSystem.DataAccessLibrary/Repositories/IBookRepository.cs
@@ -11,6 +11,8 @@ namespace LibraryManagementSystem.DataAccessLibrary.Repositories
         IEnumerable<Book> GetBooksByAuthor(string authorName);
         IEnumerable<Book> GetBooksByGenre(string genreName);
         IEnumerable<Book> GetAllBooks();
+        IEnumerable<Book> GetBorrowedBooksByMemberId(int memberId);
+        IEnumerable<Book> GetAllBorrowedBooks();
         void UpdateBook(Book book);
         void DeleteBookById(int bookId);
     }
