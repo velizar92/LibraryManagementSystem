8c08d9ee2db7cb2440f58e17626db626376f2998
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2025-01-26 19:18:09 +0200

    Set  BorrowDate and ReturnDate to be nullable in the model

--- LibraryManagementSystem.ModelsLibrary/BorrowedBooks.cs
+++ LibraryManagementSystem.ModelsLibrary/BorrowedBooks.cs
@@ -3,8 +3,8 @@
     public class BorrowedBooks : BaseDeletableEntity
     {
         public int Id { get; set; }
-        public DateTime BorrowDate { get; set; }
-        public DateTime ReturnDate { get; set; }
+        public DateTime? BorrowDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
 
         public int BookId { get; set; }
         public Book Book { get; set; }
