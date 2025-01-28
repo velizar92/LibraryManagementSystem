0374e3741fd50be01139acaa6930866ce6c04826
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2025-01-28 19:25:19 +0200

    Add IMemberService.cs and IBookService.cs in Services project dll

---
+++ LibraryManagementSystem.ServicesLibrary/Services/IBookService.cs
@@ -0,0 +1,19 @@
+﻿using LibraryManagementSystem.ModelsLibrary;
+
+namespace LibraryManagementSystem.ServicesLibrary.Services
+{
+    public interface IBookService
+    {
+        void AddBook(Book book);
+        Book GetBookById(int bookId);
+        Book GetBookByTitle(string bookTitle);
+        Book GetBookFullInfoById(int bookId);
+        IEnumerable<Book> GetBooksByAuthor(string authorName);
+        IEnumerable<Book> GetBooksByGenre(string genreName);
+        IEnumerable<Book> GetAllBooks();
+        IEnumerable<Book> GetBorrowedBooksByMemberId(int memberId);
+        IEnumerable<Book> GetAllBorrowedBooks();
+        void UpdateBook(Book book);
+        void DeleteBookById(int bookId);
+    }
+}
---
+++ LibraryManagementSystem.ServicesLibrary/Services/IMemberService.cs
@@ -0,0 +1,17 @@
+﻿using LibraryManagementSystem.ModelsLibrary;
+
+namespace LibraryManagementSystem.ServicesLibrary.Services
+{
+    public interface IMemberService
+    {
+        void AddMember(Member member);
+        Member GetMemberById(int memberId);
+        Member GetMemberByEmail(string memberEmail);
+        Member GetMemberFullInfoById(int memberId);
+        IEnumerable<Member> GetMembersByFirstName(string memberFirstName);
+        IEnumerable<Member> GetMembersByLastName(string memberLastName);
+        IEnumerable<Member> GetAllMembers();
+        void UpdateMember(Member member);
+        void DeleteMemberById(int memberId);
+    }
+}
