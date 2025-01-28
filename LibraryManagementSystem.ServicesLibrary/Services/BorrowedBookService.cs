511a97fc20274d30ba9e582a312ebfbad738bf2c
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2025-01-28 19:25:45 +0200

    Implement IBorrowedBookService interface

---
+++ LibraryManagementSystem.ServicesLibrary/Services/BorrowedBookService.cs
@@ -0,0 +1,39 @@
+﻿using LibraryManagementSystem.DataAccessLibrary.Repositories;
+
+namespace LibraryManagementSystem.ServicesLibrary.Services
+{
+    public class BorrowedBookService : IBorrowedBookService
+    {
+        private IBorrowedBookRepository _borrowedBookRepository;
+
+        public BorrowedBookService(IBorrowedBookRepository borrowedBookRepository)
+        {
+            _borrowedBookRepository = borrowedBookRepository;
+        }
+
+        public void BorrowBook(int bookId, int memberId, DateTime? borrowDate)
+        {
+            if (bookId < 0)
+            {
+                throw new ArgumentException($"The id is negative.", nameof(bookId));
+            }
+
+            if (memberId < 0)
+            {
+                throw new ArgumentException($"The id is negative.", nameof(memberId));
+            }
+
+            _borrowedBookRepository.BorrowBook(bookId, memberId, borrowDate);
+        }
+
+        public void ReturnBook(int borrowId, DateTime? returnDate)
+        {
+            if (borrowId < 0)
+            {
+                throw new ArgumentException($"The id is negative.", nameof(borrowId));
+            }
+
+            _borrowedBookRepository.ReturnBook(borrowId, returnDate);
+        }
+    }
+}
---
+++ LibraryManagementSystem.ServicesLibrary/Services/IBorrowedBookService.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryManagementSystem.ServicesLibrary.Services
+{
+    public interface IBorrowedBookService
+    {
+        void BorrowBook(int bookId, int memberId, DateTime? borrowDate);
+        void ReturnBook(int borrowId, DateTime? returnDate);
+    }
+}
