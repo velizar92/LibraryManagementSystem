2308975aee9de7e75de94e707ec674bd6d4dc1ef
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2025-01-30 20:45:06 +0200

    Add ConnectionStrings class that contains the connection strings of the app.

---
+++ LibraryManagementSystem.UI/ConnectionStrings.cs
@@ -0,0 +1,9 @@
+﻿using System.Configuration;
+
+namespace LibraryManagementSystem.UI
+{
+    public static class ConnectionStrings
+    {
+        public static string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnectionString"].ConnectionString;
+    }
+}
