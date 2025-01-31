c075c1af499493f80d0f3a7f99ead5f239cad3f7
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2025-01-31 21:15:25 +0200

    Make UpdateBookForm

---
+++ LibraryManagementSystem.UI/Forms/UpdateMemberForm.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace LibraryManagementSystem.UI.Forms
+{
+    partial class UpdateMemberForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblUpdateBook = new Label();
+            btnUpdateBook = new Button();
+            lblFirstName = new Label();
+            lblPhoneNumber = new Label();
+            lblId = new Label();
+            lblLastName = new Label();
+            lblEmail = new Label();
+            txtPhoneNumber = new TextBox();
+            txtEmail = new TextBox();
+            txtLastName = new TextBox();
+            txtFirstName = new TextBox();
+            lblUpdateMember = new Label();
+            txtId = new TextBox();
+            SuspendLayout();
+            // 
+            // lblUpdateBook
+            // 
+            lblUpdateBook.AutoSize = true;
+            lblUpdateBook.Font = new Font("Segoe UI", 28.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblUpdateBook.Location = new Point(178, -108);
+            lblUpdateBook.Margin = new Padding(10, 0, 10, 0);
+            lblUpdateBook.Name = "lblUpdateBook";
+            lblUpdateBook.Size = new Size(299, 62);
+            lblUpdateBook.TabIndex = 32;
+            lblUpdateBook.Text = "Update Book";
+            // 
+            // btnUpdateBook
+            // 
+            btnUpdateBook.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnUpdateBook.Location = new Point(127, 617);
+            btnUpdateBook.Name = "btnUpdateBook";
+            btnUpdateBook.Size = new Size(230, 55);
+            btnUpdateBook.TabIndex = 31;
+            btnUpdateBook.Text = "Update";
+            btnUpdateBook.UseVisualStyleBackColor = true;
+            btnUpdateBook.Click += btnUpdateBook_Click;
+            // 
+            // lblFirstName
+            // 
+            lblFirstName.AutoSize = true;
+            lblFirstName.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblFirstName.Location = new Point(30, 199);
+            lblFirstName.Name = "lblFirstName";
+            lblFirstName.Size = new Size(151, 38);
+            lblFirstName.TabIndex = 30;
+            lblFirstName.Text = "First Name";
+            // 
+            // lblPhoneNumber
+            // 
+            lblPhoneNumber.AutoSize = true;
+            lblPhoneNumber.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPhoneNumber.Location = new Point(30, 512);
+            lblPhoneNumber.Name = "lblPhoneNumber";
+            lblPhoneNumber.Size = new Size(206, 38);
+            lblPhoneNumber.TabIndex = 29;
+            lblPhoneNumber.Text = "Phone Number";
+            // 
+            // lblId
+            // 
+            lblId.AutoSize = true;
+            lblId.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblId.Location = new Point(30, 91);
+            lblId.Name = "lblId";
+            lblId.Size = new Size(40, 38);
+            lblId.TabIndex = 28;
+            lblId.Text = "Id";
+            // 
+            // lblLastName
+            // 
+            lblLastName.AutoSize = true;
+            lblLastName.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblLastName.Location = new Point(30, 300);
+            lblLastName.Name = "lblLastName";
+            lblLastName.Size = new Size(147, 38);
+            lblLastName.TabIndex = 27;
+            lblLastName.Text = "Last Name";
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblEmail.Location = new Point(30, 397);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(83, 38);
+            lblEmail.TabIndex = 26;
+            lblEmail.Text = "Email";
+            // 
+            // txtPhoneNumber
+            // 
+            txtPhoneNumber.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtPhoneNumber.Location = new Point(30, 553);
+            txtPhoneNumber.Multiline = true;
+            txtPhoneNumber.Name = "txtPhoneNumber";
+            txtPhoneNumber.Size = new Size(438, 46);
+            txtPhoneNumber.TabIndex = 25;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtEmail.Location = new Point(30, 438);
+            txtEmail.Multiline = true;
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(438, 46);
+            txtEmail.TabIndex = 24;
+            // 
+            // txtLastName
+            // 
+            txtLastName.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtLastName.Location = new Point(30, 341);
+            txtLastName.Multiline = true;
+            txtLastName.Name = "txtLastName";
+            txtLastName.Size = new Size(438, 46);
+            txtLastName.TabIndex = 23;
+            // 
+            // txtFirstName
+            // 
+            txtFirstName.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtFirstName.Location = new Point(30, 240);
+            txtFirstName.Multiline = true;
+            txtFirstName.Name = "txtFirstName";
+            txtFirstName.Size = new Size(438, 46);
+            txtFirstName.TabIndex = 22;
+            // 
+            // lblUpdateMember
+            // 
+            lblUpdateMember.AutoSize = true;
+            lblUpdateMember.Font = new Font("Segoe UI", 28.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblUpdateMember.Location = new Point(30, 9);
+            lblUpdateMember.Margin = new Padding(10, 0, 10, 0);
+            lblUpdateMember.Name = "lblUpdateMember";
+            lblUpdateMember.Size = new Size(369, 62);
+            lblUpdateMember.TabIndex = 33;
+            lblUpdateMember.Text = "Update Member";
+            // 
+            // txtId
+            // 
+            txtId.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtId.Location = new Point(30, 132);
+            txtId.Multiline = true;
+            txtId.Name = "txtId";
+            txtId.Size = new Size(438, 46);
+            txtId.TabIndex = 21;
+            // 
+            // UpdateMemberForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(511, 698);
+            Controls.Add(lblUpdateMember);
+            Controls.Add(lblUpdateBook);
+            Controls.Add(btnUpdateBook);
+            Controls.Add(lblFirstName);
+            Controls.Add(lblPhoneNumber);
+            Controls.Add(lblId);
+            Controls.Add(lblLastName);
+            Controls.Add(lblEmail);
+            Controls.Add(txtPhoneNumber);
+            Controls.Add(txtEmail);
+            Controls.Add(txtLastName);
+            Controls.Add(txtFirstName);
+            Controls.Add(txtId);
+            Name = "UpdateMemberForm";
+            Text = "UpdateMemberForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblUpdateBook;
+        private Button btnUpdateBook;
+        private Label lblFirstName;
+        private Label lblPhoneNumber;
+        private Label lblId;
+        private Label lblLastName;
+        private Label lblEmail;
+        private TextBox txtPhoneNumber;
+        private TextBox txtEmail;
+        private TextBox txtLastName;
+        private TextBox txtFirstName;
+        private Label lblUpdateMember;
+        private TextBox txtId;
+    }
+}
---
+++ LibraryManagementSystem.UI/Forms/UpdateMemberForm.cs
@@ -0,0 +1,69 @@
+﻿using LibraryManagementSystem.DataAccessLibrary.Repositories;
+using LibraryManagementSystem.ModelsLibrary;
+using LibraryManagementSystem.ServicesLibrary.Services;
+
+namespace LibraryManagementSystem.UI.Forms
+{
+    public partial class UpdateMemberForm : Form
+    {
+        private readonly MemberService _memberService;
+
+        public event EventHandler<MemberEventArgs> UpdatedMember;
+
+
+        public UpdateMemberForm()
+        {
+            InitializeComponent();
+
+            _memberService = new MemberService(new MemberRepository(ConnectionStrings.connectionString));
+        }
+
+        public string Id
+        {
+            get { return txtId.Text; }
+            set { txtId.Text = value; }
+        }
+
+        public string FirstName
+        {
+            get { return txtFirstName.Text; }
+            set { txtFirstName.Text = value; }
+        }
+
+        public string LastName
+        {
+            get { return txtLastName.Text; }
+            set { txtLastName.Text = value; }
+        }
+
+        public string Email
+        {
+            get { return txtEmail.Text; }
+            set { txtEmail.Text = value; }
+        }
+
+        public string PhoneNumber
+        {
+            get { return txtPhoneNumber.Text; }
+            set { txtPhoneNumber.Text = value; }
+        }
+
+        private void btnUpdateBook_Click(object sender, EventArgs e)
+        {
+            Member member = new Member
+            {
+                Id = Convert.ToInt32(txtId.Text),
+                FirstName = txtFirstName.Text,
+                LastName = txtLastName.Text,
+                Email = txtEmail.Text,
+                PhoneNumber = txtPhoneNumber.Text,
+            };
+
+            _memberService.UpdateMember(member);
+
+            UpdatedMember?.Invoke(this, new MemberEventArgs(member));
+
+            Close();
+        }
+    }
+}
