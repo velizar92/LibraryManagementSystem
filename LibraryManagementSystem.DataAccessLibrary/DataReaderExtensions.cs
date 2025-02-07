5afd4e41b2f63862f246395a95349dfffab761ad
Velizar Gerasimov <Velizar.Gerasimov@komaxgroup.com>
2025-02-07 19:12:05 +0200

    Add DataReaderExtensions.cs for separating get data logic in one place

---
+++ LibraryManagementSystem.DataAccessLibrary/DataReaderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using LibraryManagementSystem.ModelsLibrary;
+using Microsoft.Data.SqlClient;
+
+namespace LibraryManagementSystem.DataAccessLibrary
+{
+    public static class DataReaderExtensions
+    {
+        public static Book GetBookData(SqlDataReader reader)
+        {
+            return new Book
+            {
+                Id = (int)reader["Id"],
+                Title = reader["Title"] != DBNull.Value ? reader["Title"].ToString() : null,
+                Author = reader["Author"] != DBNull.Value ? reader["Author"].ToString() : null,
+                Genre = reader["Genre"] != DBNull.Value ? reader["Genre"].ToString() : null,
+                PublishedYear = reader["PublishedYear"] != DBNull.Value ? reader["PublishedYear"].ToString() : null,
+            };
+        }
+    }
+}
